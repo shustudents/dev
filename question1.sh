c4addd2e2e9bf4a3bdb153e5b9098194e4e3c850
henry Gallo <henrygallo@henrys-MacBook-Pro.local>
2017-06-01 10:53:51 +0100

    hgallo - 2

--- question1.sh
+++ question1.sh
@@ -3,4 +3,5 @@ printf "please enter your name: "
 read NAME
 printf "Your name is $NAME\n"
 # just a comment
+# another question
 exit 0
--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-printf "please enter your name: "
-read NAME
-printf "Your name is $NAME\n"
-# just a comment
-exit 0
